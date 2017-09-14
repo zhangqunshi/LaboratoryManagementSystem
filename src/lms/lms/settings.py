a22e3c33e11151a8413bc2a81fb9303b241b73a1
zhangqunshi <zhangqunshi@126.com>
2017-09-14 12:40:14 +0800

    change to mysql database

--- src/lms/lms/settings.py
+++ src/lms/lms/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'lms.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lms',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
