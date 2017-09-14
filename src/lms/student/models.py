7b629001ff3b5c74bc30a79a080c84b18110e79b
zhangqunshi <zhangqunshi@126.com>
2017-09-14 12:29:37 +0800

    create student model

--- src/lms/student/models.py
+++ src/lms/student/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+    student_num = models.CharField(primary_key=True, max_length=50)
+    student_name = models.CharField(max_length=30)
+    phone = models.CharField(max_length=20, null=True, blank=True)
+    email = models.CharField(max_length=100, null=True, blank=True)
+
+    class Meta:
+        db_table = 't_student'
+
+    def __str__(self):
+        return 'num=%s, name=%s' % (self.student_num, self.student_name)
