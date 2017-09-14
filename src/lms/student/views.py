ad8260da9b4d7bae3881af2a6ead6fb9d818c43e
zhangqunshi <zhangqunshi@126.com>
2017-09-14 13:20:56 +0800

    URL mapping for student index

--- src/lms/student/views.py
+++ src/lms/student/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    """
+    Get all student information
+    """
+    return HttpResponse("Student Index")
