da60a1c692412d58ff79cd8bc70d0b0453a6f67a
zhangqunshi <zhangqunshi@126.com>
2017-09-21 15:56:05 +0800

    add api prefix uri for all api, in order to identify them in nginx.

--- src/backend/lms/lms/urls.py
+++ src/backend/lms/lms/urls.py
@@ -27,7 +27,7 @@ router.register(r'groups', GroupViewSet)
 router.register(r'staffs', StaffViewSet)
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    url(r'^api/', include(router.urls)),
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
